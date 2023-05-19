f9af3622903f7e924a7fa8893e166636d1ecc865
Jieun Oh <0909oje@naver.com>
2023-05-19 14:47:24 +0900

    feat: 기본 세팅

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Timer } from "./components/Timer";
+import { Flex } from "./components/Common";
+
+function App() {
+  return (
+    <Flex align="center" style={{ height: "100vh" }}>
+      <Timer />
+    </Flex>
+  );
+}
+
+export default App;
